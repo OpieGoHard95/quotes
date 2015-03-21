0ae6a9f62eff90a80c0d02e4fd3160f6f6c26349
OpieGoHard95 <jsmith2@saintjoe.edu>
2015-03-20 21:07:28 -0400

    Commented code to list problems I am having

--- quotes.js
+++ quotes.js
@@ -2,5 +2,12 @@
 // Chapter Nine exercise Set problem Two
 // March 20, 2015
 
+// This defines the text to be altered
 var text = "'I'm the cook,' he said, 'it's my job.'"
+
+// Print statement where the change happens
+// First iteration =  console.log(text.replace(/\W'/, "DubQuote"));
+// 	This puts the words DubQuote right after cook, replacing the comma
+//	I still need to figure out how to get a quotation mark to appear
+//	I also need it to not replace the comma
 console.log(text.replace(/\W'/, "DubQuote"));
