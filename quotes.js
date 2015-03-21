5af2524060cab5b2abd5156161dfdff6f07747be
OpieGoHard95 <jsmith2@saintjoe.edu>
2015-03-20 21:40:25 -0400

    Third attempt at the problem

--- quotes.js
+++ quotes.js
@@ -11,7 +11,13 @@ var text = "'I'm the cook,' he said, 'it's my job.'"
 //	I still need to figure out how to get a quotation mark to appear
 //	I also need it to not replace the comma
 
-// Second iteration = console.log(text.replace(/\W'?, "\""));
-// 	This putes the " right after the cook, still replacing the comma
+// Second iteration = console.log(text.replace(/\W'/, "\""));
+// 	This puts the " right after the cook, still replacing the comma
 //	I still need to figure out how to not replace the comma
-console.log(text.replace(/\W'/, "\""));
+
+// Third iteratiopn = console.log(text.replace(/'\W/, "\""));
+//	This puts the " right before the h in he, replacing the space
+//	This did nothing but move my problem to a different place in the code
+//	The hints in the book mentions $1 and $2 however I am not sure
+//	  how to use those correctly nor how they apply in this situation
+console.log(text.replace(/'\W/, "\""));
