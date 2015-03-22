8c94d748f1a84cf37eb0049e4db73c5bb1bc1f24
OpieGoHard95 <jsmith2@saintjoe.edu>
2015-03-22 16:26:36 -0400

    Finished and solved the problem

--- quotes.js
+++ quotes.js
@@ -20,4 +20,14 @@ var text = "'I'm the cook,' he said, 'it's my job.'"
 //	This did nothing but move my problem to a different place in the code
 //	The hints in the book mentions $1 and $2 however I am not sure
 //	  how to use those correctly nor how they apply in this situation
-console.log(text.replace(/'\W/, "\""));
+
+// Fourth iteration = console.log(text.replace(/\W'|'\W/, "\""));
+// 	This does the same thing as the other one, however this was after
+// 	looking at the hints inside the book.
+// 	I am going to now look and see more about the $1 and $2
+
+// Fifth iteration = console.log(text.replace(/(^|\W)'|'(\W|$)/g, '$1"$2'));\W'|'\W/, "\""));
+// 	This iteration does work however i had to do a ton of googling to
+// 	figure it out
+
+console.log(text.replace(/(^|\W)'|'(\W|$)/g, '$1"$2'));
